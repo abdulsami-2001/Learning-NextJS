963653c3e3f730ef9bfd9fe41a28f21afd2c28d1
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2022-08-04 03:03:32 +0500

    api update , blog fetching
    existing blog, getblog api updation
    fetching those updated api on blog component ( page )

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,10 +1,40 @@
 import * as fs from 'fs'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  
+  let data = await fs.promises.readdir('BlogPostData/')
+
+  let file;
+  let allBlogs = []
+
+  
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    file =  await fs.promises.readFile(`BlogPostData/${item}`, 'utf-8')
+    allBlogs.push( JSON.parse(file))
+    
+  }
+
+  res.status(200).json(allBlogs)
+
+  
+  
+  /*
+    data.map(async (item)=>{
+      file =  await fs.promises.readFile(`BlogPostData/${item}`, 'utf-8')
+      allBlogs.push( JSON.parse(file))
+      
+    })
+  */
+  
+  /*
+  
   fs.readdir(`BlogPostData`, (err, data) => {
     if (err) {
       res.status(500).json({err:'Internal Server Erro'})
     }
     res.status(200).json(data)
   })
+  */
+  
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import STYLES from './BlogStyles.module.css'
+import Link from 'next/link'
 
 const Blog = () => {
+
+  const [Blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const options = { method: 'GET' };
+
+    fetch('http://localhost:3000/api/blogs', options)
+      .then(response => response.json())
+      .then(response => {
+
+        console.log(response)
+        setBlogs(response)
+      }
+      )
+      .catch(err =>
+        console.error(err)
+      );
+  }, [])
+
   return (
-    <div>Blog</div>
+    <>
+      {
+        Blogs.map((blogItem) => {
+          return (<div key={Math.random()} className={STYLES.mainCont} >
+            <Link href={`blog/${blogItem.slug}`} >
+              <h2>{blogItem.title}</h2>
+            </Link>
+              <p>{blogItem.content}</p>
+          </div>)
+        })
+      }
+    </>
   )
 }
 
